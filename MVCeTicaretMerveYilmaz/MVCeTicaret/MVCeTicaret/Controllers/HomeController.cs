6d28c4f281fbf1038701b8fa005cc591d7892657
Merivus <49957332+Merivus@users.noreply.github.com>
2019-07-02 03:44:02 +0300

    Spor Malzemeleri
    Projenin son hali...
    Database script'i ve proje içeriğinde yer alan resimler de yüklenmiştir...

---
+++ MVCeTicaretMerveYilmaz/MVCeTicaret/MVCeTicaret/Controllers/HomeController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using MVCeTicaret.Models;
+
+namespace MVCeTicaret.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+            Context db = new Context();
+
+            TempData["FitnessMalzemeleri"] = db.Products.Where(x => x.SubCategoryID == 1).OrderBy(x => Guid.NewGuid()).Take(3).ToList();
+            TempData["YazSporlari"] = db.Products.Where(x => x.SubCategoryID == 3).OrderBy(x => Guid.NewGuid()).Take(3).ToList();
+            TempData["KampMalzemeleri"] = db.Products.Where(x => x.SubCategoryID == 4).OrderBy(x => Guid.NewGuid()).Take(3).ToList();
+
+            return View();
+        }
+    }
+}
